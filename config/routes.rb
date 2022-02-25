1675e6c044c553ce80d22394b74175686f1ac559
= <=>
2022-02-24 21:35:01 -0300

    Created user model and sing up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :contacts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
 
   get "about", to: "static_pages#about"
   get "contact_us", to: "static_pages#contact_us"
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 
 end
