2ce02797b2c3c53dd2af464fe4b6ab2b80aa3742
= <=>
2022-02-27 00:19:52 -0300

    Added session clontroler to mange user status and added login and logout functionalitys

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    redirect_to user_path(current_user) if user_singed_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sing_in(user)
+      redirect_to user_path(user)
+    else  
+      flash.now[:danger] = "Invalid email or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    sing_out
+    flash[:success] = 'Logged out'
+    redirect_to login_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
 
   get "about", to: "static_pages#about"
   get "contact_us", to: "static_pages#contact_us"
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete "log_out", to: 'sessions#destroy'
+
 
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
 end
